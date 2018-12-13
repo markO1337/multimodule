ea01e82c2891fbde93ed041b6b3f4a3178a7dc7c
markO <mark.bus.it@gmail.com>
2018-12-13 19:46:25 +0100

    routing works

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {RouterModule, Routes} from '@angular/router';
+import {LoginComponent} from './login/login.component';
+
+const secondaryRoutes: Routes = [
+  { path: 'login',  component: LoginComponent }
+];
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(secondaryRoutes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
